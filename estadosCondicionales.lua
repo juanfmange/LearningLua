40230839cf27c08e51c7b3d66de45d9eadd2882a
Juan Fco. Mange <juanfmange@gmail.com>
2023-03-29 11:53:39 -0600

    estados condicionales en lua

---
+++ estadosCondicionales.lua
@@ -0,0 +1,42 @@
+--[[
+== equivalente
+< menor que
+> mayor que
+<= menor o igual
+>= mayor o igual
+~= no equivalente
+--]]
+
+local edad = 10
+
+-- if
+if edad >= 18 then
+  print("mayor de edad")
+end
+
+--elseif y else
+
+edad = 20
+
+if edad > 18 then
+  print("mayo de edad")
+elseif edad == 18 then
+  print("apenitas")
+else
+  print("eres un bebe")
+end
+
+-- comparacion de booleanos
+local estaVivo = true
+
+if estaVivo then
+  print("bendito dios")
+end
+
+
+-- comparacion de strings
+local name = "juanelo"
+
+if name ~= "juanelo" then
+  print("no eres juanelo")
+end
