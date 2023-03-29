31e17f25f00a77098272887117958c5ce6d71bd1
Juan Fco. Mange <juanfmange@gmail.com>
2023-03-29 11:41:30 -0600

    learning lua

---
+++ commentsAndPrint.lua
@@ -0,0 +1,8 @@
+-- Comentario en lua
+print("holis") --comentario 
+
+--[[
+comentario
+multi
+linea
+]]
---
+++ variables.lua
@@ -0,0 +1,24 @@
+-- Tipos de variables
+local x = 10 -- numero
+local name = "Juanito" -- string
+local prendido = true -- booleano
+local y = nil -- sin valor o valor invalido
+
+print(x)
+print(name)
+print(prendido)
+print(y)
+
+-- incremento en numero
+local n = 1
+n = n + 1
+print(n) -- 2
+
+-- string
+-- concatenar strings
+
+local frase = "hola"
+local final = "adios"
+
+print(frase .. " Juanito, " .. final) -- hola juanito, adios
+print("Hola".." mundo")
